7f7843c2fb2d7277e96871fda39431a9bacb64aa
JSander1808 <justin.sanderleon@icloud.com>
2022-10-09 16:44:10 +0200

    Initial commit

---
+++ src/main/java/rembel/de/rembel/Rembel.java
@@ -0,0 +1,17 @@
+package rembel.de.rembel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rembel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
